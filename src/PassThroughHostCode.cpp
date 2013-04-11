f0088834c3f89ce241d9a50701522bd80b306458
Conghui He <heconghui@gmail.com>
2013-04-11 21:43:09 +0800

    encapsulate the stuff into functions

--- src/PassThroughHostCode.cpp
+++ src/PassThroughHostCode.cpp
@@ -13,95 +13,39 @@
 using namespace std;
 void get_amplitude(float *A, int n);
 bool passVerify(float *a, float *b, size_t size);
+void allocArray(float **Pt_array, float **Pt_fpga_array, size_t n_array, size_t n_elem);
+void deleteArray(float **Pt_array, float **Pt_fpga_array, size_t size);
+void stream2FPGA(max_device_handle_t *device);
+void runAndCheck(max_device_handle_t *device, float **Pt_array, float **Pt_fpga_array, float *A, size_t size, int n_iter);
+void initFPGA(max_maxfile_t **maxfile, max_device_handle_t **device, const char *device_name);
 
 int main(int argc, char* argv[])
 {
   char *device_name = (argc==2 ? argv[1] : NULL);
-  max_maxfile_t* maxfile;
-  max_device_handle_t* device;
-  int status = 0;
+  max_maxfile_t       *maxfile;
+  max_device_handle_t *device;
+  float               *Pt_array[g_niter + 2];
+  float               *Pt_fpga_array[g_niter + 2];
+  float                A[g_niter];
 
   cout << "Opening and configuring FPGA." << endl;
+  initFPGA(&maxfile, &device, device_name);
 
-  maxfile = max_maxfile_init_PassThrough();
-  device = max_open_device(maxfile, device_name);
-  max_set_terminate_on_error(device);
-
-  cout << "Generating data to send to FPGA." << endl;
-  float *Pt_array[g_niter + 2];
-  float *Pt_fpga_array[g_niter + 2];
-
-  for (int i = 0; i < g_niter + 2; i++) {
-    Pt_array[i] = new float[g_size];
-    Pt_fpga_array[i] = new float[g_size];
-  }
-  //float *Pt        = new float[g_size];
-  //float *PtM1      = new float[g_size];
-  //float *PtP1      = new float[g_size];
-  //float *Pt_fpga   = new float[g_size];
-  //float *PtM1_fpga = new float[g_size];
-  //float *PtP1_fpga = new float[g_size];
-  float *A         = new float[g_niter];
+  allocArray(Pt_array, Pt_fpga_array, g_niter + 2, g_size);
   get_amplitude(A, g_niter);
 
-  // init array
-  for (int i = 0; i < g_size; i++) {
-    //Pt_array[1][i] = Pt_fpga_array[1][i] = 0;
-    //Pt_array[0][i] = Pt_fpga_array[0][i] = 0;
-    Pt_array[1][i] = Pt_fpga_array[1][i] = (float)rand() / 1000000;
-    Pt_array[0][i] = Pt_fpga_array[0][i] = (float)rand() / 1000000;
-  }
-
   cout << "Streaming data to/from FPGA." << endl;
-  max_set_scalar_input_f(device, "PassThroughKernel.w3", w3, FPGA_A);
-  max_set_scalar_input_f(device, "PassThroughKernel.w2", w2, FPGA_A);
-  max_set_scalar_input_f(device, "PassThroughKernel.w1", w1, FPGA_A);
-  max_set_scalar_input_f(device, "PassThroughKernel.w0", w0, FPGA_A);
-  max_set_scalar_input_f(device, "PassThroughKernel.dt", g_dt, FPGA_A);
-  max_set_scalar_input_f(device, "PassThroughKernel.c",  g_c, FPGA_A);
-  max_set_scalar_input_f(device, "PassThroughKernel.dx", g_dx, FPGA_A);
-  max_set_scalar_input_f(device, "PassThroughKernel.xMax", g_nx, FPGA_A);
-  max_set_scalar_input_f(device, "PassThroughKernel.yMax", g_ny, FPGA_A);
-  max_set_scalar_input_f(device, "PassThroughKernel.zMax", g_nz, FPGA_A);
-
-  max_set_runtime_param(device, "PassThroughKernel.nx_offset", g_nx);
-  max_set_runtime_param(device, "PassThroughKernel.nxy_offset", g_nx * g_ny);
-  max_upload_runtime_params(device, FPGA_A);
+  stream2FPGA(device);
 
   cout << "Begin to execute function: max_run" << endl;
-  size_t n_bytes = g_size * sizeof(float);
-  float *hello = new float[g_size];
-  for (int i = 1; i < g_niter + 1; i++) {
-    cout << i - 1 << "st run" << endl;
-
-    max_run(device,
-        max_input("Pt_stream", Pt_fpga_array[i], n_bytes),
-        max_input("PtM1_stream", Pt_fpga_array[i-1], n_bytes),
-        max_output("PtP1_stream", Pt_fpga_array[i+1], n_bytes),
-        max_runfor("PassThroughKernel", g_size),
-        max_end());
-
-    acoustic(Pt_array[i], Pt_array[i-1], Pt_array[i+1]);
-
-    Pt_array[i][at(0, g_ny/2, g_nx/2, g_ny, g_nx)]      += A[i-1];
-    Pt_fpga_array[i][at(0, g_ny/2, g_nx/2, g_ny, g_nx)] += A[i-1];
-
-    printf("Checking data read from FPGA.\n");
-    if (passVerify(Pt_array[i+1], Pt_fpga_array[i+1], g_size))
-      cout <<"Test failed." << endl;
-    else
-      cout << "Test passed OK!" << endl;
-
-    cout << endl;
-  }
-
+  runAndCheck(device, Pt_array, Pt_fpga_array, A, g_size, g_niter);
 
   cout << "Shutting down" << endl;
   max_close_device(device);
   max_destroy(maxfile);
 
-  return status;
-
+  deleteArray(Pt_array, Pt_fpga_array, g_niter + 2);
+  return 0;
 }
 
 /**
@@ -131,7 +75,7 @@ bool passVerify(float *a, float *b, size_t size)
   bool ret = true;
   const float epsilon = 1e-23;
 
-  for(int i = 0; i < size; i++) {
+  for(size_t i = 0; i < size; i++) {
     if (abs((a[i] - b[i]) / a[i]) > epsilon) {
       ret = false;
       cout << setw(10) << i << setw(20) << a[i] << setw(20) << b[i] << endl;
@@ -139,4 +83,79 @@ bool passVerify(float *a, float *b, size_t size)
   }
 
   return ret;
+}
+
+void allocArray(float **Pt_array, float **Pt_fpga_array,
+               size_t n_array, size_t n_elem)
+{
+  for (size_t i = 0; i < n_array; i++) {
+    Pt_array[i]      = new float[n_elem];
+    Pt_fpga_array[i] = new float[n_elem];
+  }
+
+  for (size_t i = 0; i < n_elem; i++) {
+    Pt_array[1][i] = Pt_fpga_array[1][i] = (float)rand() / 1000000;
+    Pt_array[0][i] = Pt_fpga_array[0][i] = (float)rand() / 1000000;
+  }
+}
+
+void deleteArray(float **Pt_array, float **Pt_fpga_array, size_t size)
+{
+  for (size_t i = 0; i < size; i++) {
+    delete [] Pt_array[i];
+    delete [] Pt_fpga_array[i];
+  }
+}
+
+void stream2FPGA(max_device_handle_t *device)
+{
+  max_set_scalar_input_f(device, "PassThroughKernel.w3", w3, FPGA_A);
+  max_set_scalar_input_f(device, "PassThroughKernel.w2", w2, FPGA_A);
+  max_set_scalar_input_f(device, "PassThroughKernel.w1", w1, FPGA_A);
+  max_set_scalar_input_f(device, "PassThroughKernel.w0", w0, FPGA_A);
+  max_set_scalar_input_f(device, "PassThroughKernel.dt", g_dt, FPGA_A);
+  max_set_scalar_input_f(device, "PassThroughKernel.c",  g_c, FPGA_A);
+  max_set_scalar_input_f(device, "PassThroughKernel.dx", g_dx, FPGA_A);
+  max_set_scalar_input_f(device, "PassThroughKernel.xMax", g_nx, FPGA_A);
+  max_set_scalar_input_f(device, "PassThroughKernel.yMax", g_ny, FPGA_A);
+  max_set_scalar_input_f(device, "PassThroughKernel.zMax", g_nz, FPGA_A);
+
+  max_set_runtime_param(device, "PassThroughKernel.nx_offset", g_nx);
+  max_set_runtime_param(device, "PassThroughKernel.nxy_offset", g_nx * g_ny);
+  max_upload_runtime_params(device, FPGA_A);
+}
+
+void runAndCheck(max_device_handle_t *device, float **Pt_array, 
+                 float **Pt_fpga_array, float *A, size_t size, int n_iter)
+{
+  size_t n_bytes = size * sizeof(float);
+  for (int i = 1; i < n_iter + 1; i++) {
+    cout << i - 1 << "st run" << endl;
+
+    // run with FPGA
+    max_run(device,
+        max_input("Pt_stream", Pt_fpga_array[i], n_bytes),
+        max_input("PtM1_stream", Pt_fpga_array[i-1], n_bytes),
+        max_output("PtP1_stream", Pt_fpga_array[i+1], n_bytes),
+        max_runfor("PassThroughKernel", size),
+        max_end());
+
+    // run with CPU
+    acoustic(Pt_array[i], Pt_array[i-1], Pt_array[i+1]);
+
+    Pt_array[i][at(0, g_ny/2, g_nx/2, g_ny, g_nx)]      += A[i-1];
+    Pt_fpga_array[i][at(0, g_ny/2, g_nx/2, g_ny, g_nx)] += A[i-1];
+
+    cout << (passVerify(Pt_array[i+1], Pt_fpga_array[i+1], size) ? 
+             "OK. Test passed" : "!!!Test Failed!!!")
+         << endl << endl;
+  }
+}
+
+void initFPGA(max_maxfile_t **maxfile, max_device_handle_t **device, 
+              const char *device_name)
+{
+  *maxfile = max_maxfile_init_PassThrough();
+  *device = max_open_device(*maxfile, device_name);
+  max_set_terminate_on_error(*device);
 }
--- src/global_var.h
+++ src/global_var.h
@@ -12,7 +12,7 @@ const int g_nx = 16;
 const int g_ny = 16;
 const int g_nz = 16;
 const int g_size = g_nx * g_ny * g_nz;
-const int g_niter = 100; /* number of iteration of process */
+const int g_niter = 10; /* number of iteration of process */
 
 
 #endif /* end of include guard: GLOBAL_VAR_H */
